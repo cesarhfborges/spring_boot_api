89739223082ccccea77923ef8ff788376aeb8b0f
César Borges <cesar_silk321@hotmail.com>
2025-12-15 13:42:36 -0300

    Movimentação do toDTO e toEntity para interface para que torne mais simples

--- src/main/java/br/com/chfb/api/dto/resp/PerfilResponse.java
+++ src/main/java/br/com/chfb/api/dto/resp/PerfilResponse.java
@@ -1,15 +1,12 @@
 package br.com.chfb.api.dto.resp;
 
 import java.time.LocalDate;
-import java.util.Set;
 
 public record PerfilResponse(
         String nome,
         String sobrenome,
         LocalDate dataNascimento,
         String cpf,
-        String rg,
-        Set<EnderecoResponse> enderecos,
-        Set<ContatoResponse> contatos
+        String rg
 ) {
 }
