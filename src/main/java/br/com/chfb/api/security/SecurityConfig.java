f243975a2d1ba3c203c7d70d2fb40c3b701f1b41
Cesar Borges <cesar_silk321@hotmail.com>
2026-06-08 17:39:03 -0300

    ajustes

--- src/main/java/br/com/chfb/api/security/SecurityConfig.java
+++ src/main/java/br/com/chfb/api/security/SecurityConfig.java
@@ -40,7 +40,10 @@ public class SecurityConfig {
                                 "/api/auth/login",
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
-                                "/swagger-ui.html"
+                                "/swagger-ui.html",
+                                "/actuator/health",
+                                "/actuator/health/liveness",
+                                "/actuator/health/readiness"
                         ).permitAll()
                         .anyRequest()
                         .authenticated()
